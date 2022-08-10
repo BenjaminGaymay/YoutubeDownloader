82ac882ed9edb69ab68e5d59b9f020eec5629d07
Benjamin <benjamin.gaymay@epitech.eu>
2022-08-10 13:29:36 +0200

    .

--- YTDownload.py
+++ YTDownload.py
@@ -6,6 +6,7 @@ from os.path import exists
 from subprocess import run
 from sys import argv
 import requests
+import re
 from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
@@ -15,15 +16,31 @@ if __name__ == "__main__":
 	ytdPath = getcwd()
 	if not exists(argv[2]):
 		makedirs(argv[2])
+
 	chdir(argv[2])
-	url = argv[1]
-	req = requests.get(url)
-	soup = BeautifulSoup(req.text, "html.parser")
+
+	if '-f' in argv:
+		file = f'{ytdPath}/Musiques - YouTube.html'
+
+		with open(file, 'r') as fd:
+			html = fd.read()
+	else:
+		url = argv[1]
+
+		req = requests.get(url)
+		html = req.text
+
+	soup = BeautifulSoup(html, "html.parser")
+
+	# musics = [f'https://www.youtube.com{url}' for url in re.findall(r'\/watch\?v=[a-zA-Z0-9_\-]{11}.+?index=\d+', html)]
+
 	musics = [
-	    "https://www.youtube.com" + music["href"].split('&')[0] for music in soup.find_all('a', href=True)
-	    if "/watch?" in music["href"]
+	    music["href"].split('&')[0] for music in soup.find_all('a', href=True)
+	    if "/watch?" in music["href"] and 'index=' in music['href']
 	]
+
 	musics = set(musics)
 	for music in musics:
 		process = run(["youtube-dl", "-x", "--audio-format", "mp3", music])
+		# print(' '.join(["youtube-dl", "-x", "--audio-format", "mp3", music]))
 	chdir(ytdPath)
--- rename.sh
+++ rename.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-directory="./Musiques"
+directory="./musics"
 
 for file in $directory/*; do
-  cleared=$(awk '{gsub(/-[a-zA-Z0-9_]+\.mp3$/, ".mp3");}1' <<< "$file")
+  cleared=$(awk '{gsub(/-[a-zA-Z0-9_]+\.mp3$/, ".mp3");}1' <<<"$file")
   echo "move $file to $cleared"
   mv "$file" "$cleared"
 done
-
